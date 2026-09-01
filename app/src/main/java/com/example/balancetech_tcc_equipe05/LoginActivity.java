137192386dacdeca6f89d8f58acf8227983361af
EduardoAmorsantana <eduardo.amorsantana@gmail.com>
2026-09-01 11:18:47 -0300

    Desenvolvimento em andamento

---
+++ app/src/main/java/com/example/balancetech_tcc_equipe05/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.balancetech_tcc_equipe05;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
--- app/src/main/java/com/example/balancetech_tcc_equipe05/MainActivity.java
+++ app/src/main/java/com/example/balancetech_tcc_equipe05/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.balancetech_tcc_equipe05;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.bem_vindo);
+
+        Button btnEntrar = findViewById(R.id.btnEntrar);
+
+        btnEntrar.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
