e8020018474c83fe426e964a2698832a75f21aa2
Jade Melo <jadesampaiomelo@gmail.com>
2026-09-05 21:52:58 -0300

    Desenvolvimento em andamento

---
+++ app/src/main/java/com/example/balancetech_tcc_equipe05/EsqueceuSenha.java
@@ -0,0 +1,15 @@
+package com.example.balancetech_tcc_equipe05;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EsqueceuSenha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.esqueceu_senha);
+    }
+}
--- app/src/main/java/com/example/balancetech_tcc_equipe05/LoginActivity.java
+++ app/src/main/java/com/example/balancetech_tcc_equipe05/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.balancetech_tcc_equipe05;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        TextView esqueciSenha = findViewById(R.id.esqueciSenha);
+
+        esqueciSenha.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, EsqueceuSenha.class);
+            startActivity(intent);
+        });
     }
 }
